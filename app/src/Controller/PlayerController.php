0d83d383d8fda9a3aa3a4d4b72ccd2235a0b35e1
Dan Abrey <danabrey@gmail.com>
2019-02-07 16:16:00 +0000

    Cache JSON responses for 1 day

--- app/src/Controller/PlayerController.php
+++ app/src/Controller/PlayerController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Service\CollegePlayerStatsScraper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class PlayerController
@@ -14,12 +17,25 @@ class PlayerController extends AbstractController
 {
     /**
      * @Route("/player/{id}")
+     * @param int $id
      * @param CollegePlayerStatsScraper $scraper
+     * @param SerializerInterface $serializer
+     * @param AdapterInterface $cache
      * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function test(int $id, CollegePlayerStatsScraper $scraper)
+    public function test(int $id, CollegePlayerStatsScraper $scraper, SerializerInterface $serializer, AdapterInterface $cache): JsonResponse
     {
-        $stats = $scraper->getPlayerStats($id);
-        return $this->json($stats);
+        $cachedItem = $cache->getItem((string)$id);
+        $cachedItem->expiresAt(new \DateTime('tomorrow', new \DateTimeZone('UTC')));
+
+        if (!$cachedItem->isHit()) {
+            $stats = $scraper->getPlayerStats($id);
+            $cachedItem->set($serializer->serialize($stats, 'json'));
+            $cache->save($cachedItem);
+        }
+
+        $json = $cachedItem->get();
+        return new JsonResponse($json, 200, [], true);
     }
 }
